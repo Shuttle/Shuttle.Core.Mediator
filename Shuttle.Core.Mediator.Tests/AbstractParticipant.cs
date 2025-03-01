2009ecaac4690305c293b1762aa6e18ac5bf1480
Eben <me@ebenroux.co.za>
2025-03-01 13:27:20 +0200

    Using `DateTimeOffset`.

--- Shuttle.Core.Mediator.Tests/AbstractParticipant.cs
+++ Shuttle.Core.Mediator.Tests/AbstractParticipant.cs
@@ -8,12 +8,12 @@ public abstract class AbstractParticipant
     public int CallCount { get; private set; }
 
     public Guid Id { get; } = Guid.NewGuid();
-    public DateTime WhenCalled { get; private set; }
+    public DateTimeOffset WhenCalled { get; private set; }
 
     public void Call()
     {
         CallCount++;
-        WhenCalled = DateTime.Now;
+        WhenCalled = DateTimeOffset.UtcNow;
     }
 
     public async Task CallAsync()
