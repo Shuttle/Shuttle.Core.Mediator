fc5a75142d26de256bcb46d73f1cef628b55c936
Eben <me@ebenroux.co.za>
2025-03-02 16:03:36 +0200

    Revert "Using `DateTimeOffset`."
    This reverts commit 2009ecaac4690305c293b1762aa6e18ac5bf1480.

--- Shuttle.Core.Mediator.Tests/AbstractParticipant.cs
+++ Shuttle.Core.Mediator.Tests/AbstractParticipant.cs
@@ -8,12 +8,12 @@ public abstract class AbstractParticipant
     public int CallCount { get; private set; }
 
     public Guid Id { get; } = Guid.NewGuid();
-    public DateTimeOffset WhenCalled { get; private set; }
+    public DateTime WhenCalled { get; private set; }
 
     public void Call()
     {
         CallCount++;
-        WhenCalled = DateTimeOffset.UtcNow;
+        WhenCalled = DateTime.Now;
     }
 
     public async Task CallAsync()
